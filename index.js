aa268758a26db807dc12deceed0382e819b5fa9a
00Shawon <mehedishawon121@gmail.com>
2025-11-12 19:52:59 +0600

    home commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+// const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("artworkDB");
+    const artworkCollection = db.collection("artworks");
+
+    //showing data in the explore home page
+    app.get("/homeArtwork", async (req, res) => {
+      const result = await artworkCollection
+        .find({ visibility: "Public" })
+        .sort({ createdAt: -1 })
+        .limit(6)
+        .toArray();
+      res.send(result);
+    });
+    //showing data in the explore artwork page
+
+    app.get("/publicArtwork", async (req, res) => {
+      const result = await artworkCollection
+        .find({ visibility: "Public" })
+        .toArray();
+      res.send(result);
+    });
+
+    //showing data in the detail artwork page
+
+    app.get("/publicArtwork/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const result = await artworkCollection.findOne({ _id: new ObjectId(id) });
+
+      res.send({ success: true, result });
+    });
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("server is running!");
+});
+
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}`);
+});

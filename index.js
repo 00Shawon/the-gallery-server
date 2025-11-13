a74a2544ff6b008c2088406a4ee6d356e1b62247
00Shawon <mehedishawon121@gmail.com>
2025-11-13 06:26:13 +0600

    functionality add artwwork

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
-
+dotenv.config();
 // middleware
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.8sp7th5.mongodb.net/artworkDB?retryWrites=true&w=majority&appName=Cluster0`;
 
 // const uri = process.env.MONGO_URI;
-
+console.log(uri);
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -25,12 +27,13 @@ async function run() {
 
     const db = client.db("artworkDB");
     const artworkCollection = db.collection("artworks");
+    const favoriteCollection = db.collection("favorites");
 
     //showing data in the explore home page
     app.get("/homeArtwork", async (req, res) => {
       const result = await artworkCollection
         .find({ visibility: "Public" })
-        .sort({ createdAt: -1 })
+        .sort({ createdAt: "asc" })
         .limit(6)
         .toArray();
       res.send(result);
@@ -54,9 +57,15 @@ async function run() {
       res.send({ success: true, result });
     });
 
+    //add artwork
+    app.post("/publicArtwork", async (req, res) => {
+      const newArtwork = req.body;
+      console.log(newArtwork);
+      const result = await artworkCollection.insertOne(newArtwork);
+      res.send(result);
+    });
 
-
-
+   
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
